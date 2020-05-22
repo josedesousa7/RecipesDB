24253b94c1708eef8ddee389ee93c434430c525a
Jose Marques <josealejandro.marques@altran.com>
2020-05-22 16:03:55 +0100

    Created model objects

---
+++ RecipesDB/RecipesDB/Model/Meal.swift
@@ -0,0 +1,15 @@
+//
+//  Meal.swift
+//  RecipesDB
+//
+//  Created by José Marques on 22/05/2020.
+//  Copyright © 2020 José Marques. All rights reserved.
+//
+
+import Foundation
+
+struct Meal : Codable {
+    let strMeal: String
+    let strMealThumb: String
+    let idMeal: String
+}
