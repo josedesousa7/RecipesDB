5f92d4f3bbc206c02134c036da962fc30143ba80
Jose Marques <josealejandro.marques@altran.com>
2020-05-22 18:06:18 +0100

    Added unit tests to validade parsing of data

--- RecipesDB/RecipesDB/Service/DataManager.swift
+++ RecipesDB/RecipesDB/Service/DataManager.swift
@@ -12,5 +12,63 @@ import RxCocoa
 import RxSwift
 
 class DataManager {
-    
+    let apiKey = "1"
+    let mealsList: BehaviorRelay <[Meal]> = BehaviorRelay(value:[])
+    let detailedMeal: BehaviorRelay <DetailedMeal?> = BehaviorRelay(value: nil)
+
+    func requestMeals(withMainIngredient ingredient:String?) {
+        var mainIngredient = String()
+        if let ingredientToSearch = ingredient {
+            mainIngredient = ingredientToSearch
+        }
+
+        let requestURL = "https://www.themealdb.com/api/json/v1/\(apiKey)/filter.php?i=\(mainIngredient)"
+        AF.request(requestURL).responseJSON{ response in
+            switch response.result {
+            case .success(let json):
+                print(json)
+                let decoder = JSONDecoder()
+                guard let data = response.data else {return}
+                do {
+                    let searchResults = try decoder.decode(MealsSearchResults.self, from: data)
+                    self.mealsList.accept(searchResults.meals)
+
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            case .failure(let error):
+                print("Error:\(error.localizedDescription)")
+            }
+
+        }
+//        Alamofire.request(requestURL).responseJSON{ response in
+//            switch response.result{
+//
+//            case .success(let JSON):
+//                let decoder = JSONDecoder()
+//                guard let data = response.data else {return}
+//                let initialMovieCount = self.moviesList.value.count
+//                do {
+//                    print("Got it, JSON:\(JSON)")
+//                    let searchResults = try decoder.decode(SearchResults.self, from: data)
+//                    print("Got Movie")
+//                    let newValue = self.moviesList.value + searchResults.Search
+//                    self.moviesList.accept(newValue)
+//                } catch let error {
+//                    print(error.localizedDescription)
+//                }
+//
+//                if (self.moviesList.value.count == initialMovieCount && page == 1){
+//                    do {
+//                        let requestError = try decoder.decode(RequestError.self, from: data)
+//                        self.moviesListRequestError.accept(requestError)
+//                    } catch let error {
+//                        print(error.localizedDescription)
+//                    }
+//                }
+//            case .failure(let error):
+//                print("Error:\(error.localizedDescription)")
+//            }
+//        }
+    }
 }
--- RecipesDB/RecipesDBTests/RecipesDBTests.swift
+++ RecipesDB/RecipesDBTests/RecipesDBTests.swift
@@ -6,9 +6,15 @@
 //
 
 import XCTest
+import Alamofire
+
+
 @testable import RecipesDB
 
 class RecipesDBTests: XCTestCase {
+
+
+    var resultList = [Meal]()
 
     override func setUpWithError() throws {
     }
@@ -16,9 +22,55 @@ class RecipesDBTests: XCTestCase {
     override func tearDownWithError() throws {
     }
 
-    func testEmptyString() {
-        let string = ""
-        XCTAssertNotNil(string)
+    func testMealListParsing() {
+        let requestURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=beef"
+        let expectation = self.expectation(description: "Parsing")
+        AF.request(requestURL).responseJSON{ response in
+            switch response.result {
+            case .success(let json):
+                let decoder = JSONDecoder()
+                guard let data = response.data else {return}
+                do {
+                    let searchResults = try decoder.decode(MealsSearchResults.self, from: data)
+                    self.resultList = searchResults.meals
+                    expectation.fulfill()
+                    print(searchResults)
+
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            case .failure(let error):
+                print("Error:\(error.localizedDescription)")
+            }
+        }
+        waitForExpectations(timeout: 20, handler: nil)
+        XCTAssertTrue(resultList.count > 0)
     }
 
+    func testMealDetailParsing () {
+        let id = "52874"
+        var mealSearched = [DetailedMeal]()
+        let requestURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=\(id)"
+        let expectation = self.expectation(description: "Parsing")
+        AF.request(requestURL).responseJSON{ response in
+            switch response.result {
+            case .success(let json):
+                let decoder = JSONDecoder()
+                guard let data = response.data else {return}
+                do {
+                    let searchResults = try decoder.decode(DetailedMealResult.self, from: data)
+                   mealSearched = searchResults.meals
+                    expectation.fulfill()
+                    print(mealSearched)
+
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            case .failure(let error):
+                print("Error:\(error.localizedDescription)")
+            }
+        }
+        waitForExpectations(timeout: 20, handler: nil)
+        XCTAssert(mealSearched.count > 0)
+    }
 }
