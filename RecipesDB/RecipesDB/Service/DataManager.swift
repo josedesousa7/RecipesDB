8774b2034a84adb94f3e3966a4cbf768552bd9f5
Jose Marques <josealejandro.marques@altran.com>
2020-05-22 16:41:40 +0100

    Created folder structure of the App and Data Manager for network requests

---
+++ RecipesDB/RecipesDB/Service/DataManager.swift
@@ -0,0 +1,16 @@
+//
+//  DataManager.swift
+//  RecipesDB
+//
+//  Created by José Marques on 22/05/2020.
+//  Copyright © 2020 José Marques. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import RxCocoa
+import RxSwift
+
+class DataManager {
+    
+}
